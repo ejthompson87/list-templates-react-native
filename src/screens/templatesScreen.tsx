3ee2ce1d22429d64760622c371f0623b642366bc
Emma Thompson <ejthompson87@gmail.com>
2022-01-22 13:57:45 +0000

    add variables file and initial colour palette

--- src/screens/templatesScreen.tsx
+++ src/screens/templatesScreen.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { colors } from "../styles/Variables";
 
 export default function TemplatesScreen() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={styles.screenTest}>
       <Text>Templates Screen</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  screenTest: {
+    flex: 1,
+    alignItems: 'center', 
+    justifyContent: 'center',
+    backgroundColor: colors.PRIMARY_COLOR,
+  }
+});
---
+++ src/styles/Variables.tsx
@@ -0,0 +1,10 @@
+export const colors = {
+    // colour palette 
+    PRIMARY_COLOR: "#19647E",
+    PRIMARY_COLOR_OPP_DARK: "#2F4858",
+    SECONDARY_COLOR: "#FFBE0B",
+    TERTIARY_COLOR: "#97B1A6",
+    MAIN_GREY: "#C9C5BA",
+};
+
+
